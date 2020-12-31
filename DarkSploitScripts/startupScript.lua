f2dc91af21db60e1876a56840a2c59f6d17c724e
1337.xyz <52249933+ArTeM09823@users.noreply.github.com>
2020-12-31 21:54:15 +0500

    Create startupScript.lua

---
+++ DarkSploitScripts/startupScript.lua
@@ -0,0 +1,8 @@
+local StarterGui = game:GetService("StarterGui")
+
+StarterGui:SetCore("SendNotification", {
+    Title = "vvml",
+    Text = "Successfuly attached!",
+    Duration = 10,
+    Button1 = "Okay",
+})
